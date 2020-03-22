fa3e49a09eaa2c0d5bd701d015b05794f14cb35c
NONAN23x <61901753+NONAN23x@users.noreply.github.com>
2020-03-22 11:54:25 +0530

    Update ProjectPy.py

--- ProjectPy.py
+++ ProjectPy.py
@@ -1,18 +1,40 @@
-print('\n\nHello User,\nThis application is under development so dont expect anything so flashy')
-print('Go ahead and type what command you would like to run\n')
-a = "Addition"
-b = "Subratction"
-c = "Multiplication"
-d = "Division\n"
-Functions = [a,b,c,d]
-print(Functions[0],',',Functions[1],',',Functions[2],',',Functions[3])
-x = (input(':'))
-y = len(x)
-if y == len(a):
-    print('Adding the given Numbers')
-    print (float(input('1:')) + float(input('2: ')))
-if y != len(a):
-    exit()
-if y == len(b):
-    print('Subtracting the given Numbers')
-    print(float(input('1:')) - float(input('2: ')))
+from math import sqrt
+def maths():
+    user_input = str(input('Which Calculation do you want to make? '))
+    if user_input == "Hypotenuse":
+        print('Input the lengths of the shorter triangle sides')
+        a = float(input('a: '))
+        b = float(input('b: '))
+        c = sqrt(a**2 + b**2)
+        print("The length of the Hypotenuse is: %.3f" %c )
+    elif user_input == "Circumference":
+        Pi = 3.14
+        r = float(input('Enter the radius of the circle: '))
+        Area = Pi * r * r
+        print('Circumference of the Circle is Defined by: %.2f' %Area)
+    elif user_input == "Square Root":
+        print('Input the number you want the square of: ')
+        num1 = float(input('a: '))
+        num = sqrt(num1)
+        print('The Square Root of the given number is: %.2f' %num)
+    elif user_input == "Cube Root":
+        print('Input the number you want the cube root of: ')
+        x = float(input("a: "))
+        y =  x ** (1. / 3)
+        print("The Cube Root of the Desired number is: %.2f" %y)
+
+
+    else:
+        print("You dumbhead!\n Choose between Hypotenuse, Circumference, Square Root, Cube Root, Addition, Subtraction, Multiplication")
+
+def loop_function():
+    i = 1
+    while i < 10:
+        maths()
+        user_input = str(input('Do you want to continue? (Y/N) '))
+        if user_input == "Y":
+            i +=1
+        else:
+            break
+
+loop_function()
