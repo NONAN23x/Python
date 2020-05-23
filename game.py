4d7cf7d9e94e3dddb6db7a9a2a3b5c566c639bd2
NONAN23x <61901753+NONAN23x@users.noreply.github.com>
2020-05-23 11:16:11 +0530

    Update game.py

--- game.py
+++ game.py
@@ -1,31 +1,41 @@
 import random, time
-print()
-print('System_Generic Rebooting...\n')
-k = 10
-time.sleep(3)
-secnum = random.randint(1,20)
-print('I have taken a number, not less than 1 and not greater than 20...\n ')
+
+print('Starting game\n')
 time.sleep(2)
+secnum = random.randint(1,20)
+print("I've taken a number bwtween 1 and 20...\n")
 
+def game():
+    for i in range(1, 10):
+        print('\nCan you huess that number?\n')
+        print('---------->')
+        guessnum = int(input())
+        if guessnum < secnum:
+            print('\nAim higher...')
+        elif guessnum > secnum:
+            print('\nAim lower...')
+        else:
+            break
 
-for i in range(1,10):
-    print('Can you guess that number???\n')
-    guess = int(input('----->\n '))
-
-    if guess < secnum:
+    if guessnum == secnum:
         time.sleep(2)
-        print('Nope aim higher...')
-    elif guess > secnum:
-        time.sleep(2)
-        print('\nWhat? Aim a little bit smaller')
+        print('Correct!!!')
+        print("But you took you " + str(i) + " tries to guess...")
     else:
-        break
+        print('Nope the number i am thinking ois' + str(secnum))
 
-if guess == secnum:
-    time.sleep(2)
-    print('\nYup, you do have brain...\n')
-    time.sleep(2)
-    print('But it took ' + str(i) + " tries for your brain to function...")
-else:
-    time.sleep(2)
-    print('Nope, the number i was thinking was actually ' + str(secnum))
+def looping():
+    game()
+    print('Wanna play again?\n    (y) or (n)')
+    des = input()
+    if des == "y":
+        looping()
+    elif des == "n":
+        quit()
+    else:
+        print('Quitting')
+        time.sleep(2)
+        quit()
+
+
+looping()
