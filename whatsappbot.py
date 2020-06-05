b2ce19d713c8a3807972b9a7c013240af4028dc0
Nomaan JamalUddin <61901753+NONAN23x@users.noreply.github.com>
2020-06-05 15:48:43 +0530

    Update whatsappbot.py

--- whatsappbot.py
+++ whatsappbot.py
@@ -4,14 +4,12 @@ account_sid = "ACc6d5a5f2b1a4938dcee19c35bdcedf79"
 auth_token = "41af7bdf7d98db72afa9c3ce5cb6cc0d"
 client = Client(account_sid, auth_token)
 
-msg = str(input("Message: "))
-person = str(input("To : "))
-
+msg = str(input("Message : "))
 
 message = client.messages.create(
                               body=msg,
                               from_='whatsapp:+14155238886',
-                              to='whatsapp:' + person
+                              to='whatsapp:+919502760173'
                           )
 
 print(message.sid)
