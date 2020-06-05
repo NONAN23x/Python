2772a792e33d0592a2aae79a89da8ac8c5d039a3
Nomaan JamalUddin <61901753+NONAN23x@users.noreply.github.com>
2020-06-05 16:17:44 +0530

    Update whatsappbot.py

--- whatsappbot.py
+++ whatsappbot.py
@@ -1,8 +1,6 @@
 from twilio.rest import Client
 
-account_sid = "ACc6d5a5f2b1a4938dcee19c35bdcedf79"
-auth_token = "41af7bdf7d98db72afa9c3ce5cb6cc0d"
-client = Client(account_sid, auth_token)
+client = Client()
 
 msg = str(input("Message : "))
 
