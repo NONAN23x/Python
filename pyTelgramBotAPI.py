6aaaf3633fbda39c16dc452261a3ea8ba9b5d021
Nomaan JamalUddin <61901753+NONAN23x@users.noreply.github.com>
2021-02-01 15:08:29 +0530

    Add files via upload

---
+++ pyTelgramBotAPI.py
@@ -0,0 +1,33 @@
+# lol
+import telebot
+import datetime
+import time
+import Scheduling
+
+bot = telebot.TeleBot("1367832954:AAFlfP275ECocvCs1arY2cT3SZdCzVY7dGc")
+user = '1388226474'
+
+
+def send_messages(message):
+    bot.send_message(chat_id=user, text=message)
+
+
+def main():
+    send_messages("Segmentation initialised")
+    while True:
+        hour = datetime.datetime.now().hour - 12 if datetime.datetime.now().hour >= 13 else datetime.datetime.now().hour
+        tmp = " PM" if datetime.datetime.now().hour >= 12 else " AM"
+        current_time = str(hour) + ':' + str(datetime.datetime.now().minute) + tmp
+        print(current_time)
+        if current_time in Scheduling.dict:
+            print(current_time)
+            send_messages(Scheduling.dict[current_time])
+            print("Message sent successfully")
+            print(current_time)
+            time.sleep(60)
+        else:
+            time.sleep(30)
+
+
+if __name__ == '__main__':
+   main()
